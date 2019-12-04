c8267e823427cf43731d2920128e87a77e7584a0
John Hughes <jpl.hughes@btinternet.com>
2019-12-04 12:19:53 +0000

    install tmux and autosuggestions correct location

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,15 +9,17 @@ fi
 machine=$1
 if [ $machine == "linux" ]; then
     sudo apt-get install zsh
+    sudo apt-get install tmux
 elif [ $machine == "mac" ]; then
     brew install zsh
+    brew install tmux
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
