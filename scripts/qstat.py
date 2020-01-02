8b092ba5782309a712b8241362e33b675c34353f
John Hughes <jpl.hughes@btinternet.com>
2020-01-02 18:15:08 +0000

    Pretty table for qstat

--- scripts/qstat.py
+++ scripts/qstat.py
@@ -4,12 +4,13 @@ import xml.dom.minidom
 import os
 from datetime import datetime
 import timeago
+from tabulate import tabulate
 
 titanx_list = ["code0", "code11", "code20", "code5"]
 gpu980_list = ["code10", "code12", "code13", "code15", "code16"]
 
 def fakeqstat(joblist):
-    print("JOB ID", '\t', "USER".ljust(8), '\t', "NAME".ljust(16), '\t', "STATE", '\t', "MACHINE".ljust(8), '\t', "TIME AGO".ljust(16))
+    table = []
     for job in joblist:
         try:
             # extract information from qstat output
@@ -37,12 +38,24 @@ def fakeqstat(joblist):
             start_datetime = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S')
             time_ago = timeago.format(start_datetime, now_datetime)
 
-            print(job_ID, '\t', user.ljust(8), '\t', name.ljust(16), '\t', state, '\t', queue_info.ljust(8), '\t', time_ago.ljust(16))
+            table.append([job_ID, user, name, state, queue_info, time_ago])
         except Exception as e:
-            print(e)
+            print(e) 
+    return table
 
-qstat_output = os.popen('qstat -u \* -xml -r')
-qstat_xml = xml.dom.minidom.parse(qstat_output)
-job_info = qstat_xml.getElementsByTagName('job_info')[0]
-job_list = job_info.getElementsByTagName('job_list')
-fakeqstat(job_list)
+servers = ["codex", "bastion"]
+full_table = []
+for server in servers:
+    # Get qstat information
+    qstat_cmd = "qstat -u \* -xml -r"
+    if server == "bastion":
+        qstat_cmd = "ssh -t johnh@192.168.6.231 '%s' 2> /dev/null" % qstat_cmd
+    qstat_output = os.popen(qstat_cmd)
+    qstat_xml = xml.dom.minidom.parse(qstat_output)
+    job_info = qstat_xml.getElementsByTagName('job_info')[0]
+    job_list = job_info.getElementsByTagName('job_list')
+    table = fakeqstat(job_list)
+    full_table.extend(table)
+
+headers = ["JOB ID", "USER", "NAME", "STATE", "MACHINE", "SUBMITTED"]
+print(tabulate(full_table, headers, tablefmt="presto"))
