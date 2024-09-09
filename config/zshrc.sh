ce0df6eb9264a64c898a4b4f7d4db836d428d85a
John Hughes <jpl.hughes@btinternet.com>
2024-09-09 15:06:47 +0000

    add micromamba setup

--- config/zshrc.sh
+++ config/zshrc.sh
@@ -26,4 +26,16 @@ cat $CONFIG_DIR/start.txt
 
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+if command -v pyenv >/dev/null; then
+  eval "$(pyenv init -)"
+fi
+
+export MAMBA_EXE='/home/johnh/.local/bin/micromamba';
+export MAMBA_ROOT_PREFIX='~/micromamba';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias micromamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+fi
+unset __mamba_setup
